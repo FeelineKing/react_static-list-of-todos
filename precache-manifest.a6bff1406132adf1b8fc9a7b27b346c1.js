7904b8e4c379808d7ff1104cd13f30834184c117
Leon <FeelineKing@gmail.com>
2019-11-03 12:05:59 +0200

    Updates

---
+++ precache-manifest.a6bff1406132adf1b8fc9a7b27b346c1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "466aad8d4efad9a704886344fe996978",
+    "url": "/react_static-list-of-todos/index.html"
+  },
+  {
+    "revision": "26879303a6e29f715e4c",
+    "url": "/react_static-list-of-todos/static/css/main.cbc560d1.chunk.css"
+  },
+  {
+    "revision": "6df4285f02b988ca3bf5",
+    "url": "/react_static-list-of-todos/static/js/2.fdcae455.chunk.js"
+  },
+  {
+    "revision": "26879303a6e29f715e4c",
+    "url": "/react_static-list-of-todos/static/js/main.e55a27f2.chunk.js"
+  },
+  {
+    "revision": "ea6f31786c2aed313835",
+    "url": "/react_static-list-of-todos/static/js/runtime~main.00529491.js"
+  }
+]);
--- src/App.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import './App.css';
-import TodoList from './components/TodoList/TodoList';
-
-export default function App() {
-  return (
-    <TodoList />
-  );
-}
